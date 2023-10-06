8ed7e8c5334b07bbb62c57f1464b4f8fb88e9869
radiochu <72332313+radiochu@users.noreply.github.com>
2023-10-06 11:50:35 -0400

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, url_for
 
 
 def create_app():
@@ -7,16 +7,15 @@ def create_app():
     parts = {
         'lowpressure': {
             'name': 'Low Tire Pressure Warning',
-            'image': 'lowpressure.jpg',
+            'image': '../static/lowpressure.jpg',
             'description': 'This illuminates when your tire pressure is low. If the lamp remains on with the engine '
                            'running or when driving, check your tire pressure as soon as possible. It will also '
                            'illuminate momentarily when you switch the ignition on to confirm the lamp is functional. '
-                           'If it does not illuminate when you switch the ignition on, or begins to flash at any '
-                           'time, have the system checked by your authorized dealer.'
+                           
         },
         'autolamps': {
             'name': 'Autolamps',
-            'image': 'autolamps.jpg',
+            'image': '../static/lighting.jpg',
             'description': 'When the lighting control is in the autolamps position, the headlamps turn on in low '
                            'light situations, or when the wipers turn on. The headlamps remain on for a period of '
                            'time after you switch the ignition off. Use the information display controls to adjust '
@@ -24,14 +23,14 @@ def create_app():
         },
         'meme': {
             'name': 'Meme',
-            'image': 'meme.jpg',
+            'image': '../static/meme.jpg',
             'description': 'The world\'s greatest meme, guaranteed.'
+                            '                                      '
         }
     }
 
-    # a simple response that returns OK
     @app.route('/')
-    def send_ok():
+    def main_page():
         return render_template("index.html")
 
     @app.route('/qr')
@@ -41,6 +40,14 @@ def create_app():
     @app.route('/details')
     def details():
         return render_template("details.html")
+
+    @app.route('/details2')
+    def details2():
+        return render_template("details2.html")
+
+    @app.route('/details3')
+    def details3():
+        return render_template("details3.html")
 
     @app.route('/scans')
     def scans():
